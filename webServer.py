cfb16e9f05848531c089d58dd7d697145128b19c
Alvin Wen <aw4064@nyu.edu>
2025-09-20 01:50:03 -0400

    commit 16

--- webServer.py
+++ webServer.py
@@ -67,7 +67,7 @@ def webServer(port=13331):
       #Fill in start
 
       #Fill in end
-      content =  "Content-Type: text/html; charset=UTF-8\r\n\r\n"
+      content = "HTTP/1.0 404 NOT FOUND\r\n Content-Type: text/html\r\n\r\n"
       # connectionSocket.send('HTTP/1.0 404 NOT FOUND\r\n'.encode())
       # connectionSocket.send("Content-Type: text/html\r\n".encode())
       connectionSocket.send(content.encode())
